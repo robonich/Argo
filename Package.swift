ee299cdb044d21368ff3b17fc9b82bd5f187e283
Nisho Matsushita <robonich@gmail.com>
2016-10-08 16:05:12 +0900

    Add Curry and Runes

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "Argo"
+    name: "Argo",
+    dependencies: [
+        .Package(url: "https://github.com/robonich/Runes.git", Version(3, 2, 2)),
+        .Package(url: "https://github.com/robonich/Curry.git", Version(2, 3, 4))
+    ]
 )
